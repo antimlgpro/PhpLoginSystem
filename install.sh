e498ef22f4f851fabba311d941958947864a82b3
antimlgpro <ludvigvonreis@hotmail.se>
2016-12-02 13:57:56 +0100

    added database "install"

--- install.sh
+++ install.sh
@@ -7,4 +7,9 @@ sudo systemctl restart apache2
 cwd=`pwd`
 cd ..
 sudo mv -v ${cwd}/* /var/www/html/
+sudo rm /var/www/html/index.html
+mysql -u root -proot -e "CREATE DATABASE Users; USE Users;
+ CREATE TABLE users (Admin VARCHAR(1),
+username VARCHAR(30),
+password CHAR(20));"
 exit
