45545448083663ca6d540570032bbd28bb006a81
antimlgpro <ludvigvonreis@hotmail.se>
2016-12-02 04:47:20 -0500

    Fixed it again

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+ #!/bin/bash
 echo $SUDO_USER
 echo "To install we need php, mysql and apache2"
 echo "Let me install them for you"
-sudo apt-get install apache2 mysql-server mysql-client php7.0 php7.0-mysql -y
+sudo apt-get install apache2 mysql-server mysql-client php7.0 php7.0-mysql -y --allow-unauthenticated
 sudo systemctl restart apache2
+cwd=`pwd`
 cd ..
-sudo mv /PhpLoginSystem-master/ /var/www/html/
+sudo mv -v ${cwd}/* /var/www/html/
 exit
