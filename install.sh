baeb889ff61b795485251bb2ea41ccacdb25ab71
antimlgpro <ludvigvonreis@hotmail.se>
2016-12-03 12:00:40 +0100

    Fixed a error in php installation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo $SUDO_USER
 echo "To install we need php, mysql and apache2"
 echo "Let me install them for you"
-sudo apt-get install apache2 mysql-server mysql-client php7.0 php7.0-mysql -y --allow-unauthenticated
+sudo apt-get install apache2 mysql-server mysql-client php7.0 php7.0-mysql libapache2-mod-php -y --allow-unauthenticated
 sudo systemctl restart apache2
 cwd=`pwd`
 cd ..
